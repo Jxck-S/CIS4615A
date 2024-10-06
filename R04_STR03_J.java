b06c73c707610ca0f6055edf829fe83da3636c8f
Jxck-S <JackHSweeney1@gmail.com>
2024-10-06 19:40:57 -0400

    Add user selected

--- R04_STR03_J.java
+++ R04_STR03_J.java
@@ -2,8 +2,8 @@
 // STR03-J. Do not encode noncharacter data as a string Given the non-compliant code below:
 
 
-BigInteger x = new BigInteger ("530500452766");
-byte [] byteArray = x. toByteArray ();
-String s = new String (byteArray);
-byteArray = s. getBytes ();
+BigInteger x = new BigInteger("530500452766");
+byte [] byteArray = x.toByteArray();
+String s = new String(byteArray);
+byteArray = s. getBytes();
 x =new BigInteger (byteArray);
--- README.md
+++ README.md
@@ -2,3 +2,20 @@
 
 ### Homework 2
 #### Tracking Java Security weaknesses using GitHub
+
+#### Required
+
+- R00
+- R02
+- R03
+- R04
+- R06
+
+
+#### User picked
+
+- R01
+- R05
+- R13
+- R14
+- R49
