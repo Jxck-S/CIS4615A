d6e04de67ca059b1937b765d69890046036a0fc5
Jxck-S <JackHSweeney1@gmail.com>
2024-10-06 19:56:56 -0400

    Revert edits merge conflict onto main

--- R00_IDS03_J.java
+++ R00_IDS03_J.java
@@ -1,15 +1,8 @@
 // 1. Rule 00. Input Validation and Data Sanitization (IDS)
 // IDS03-J. Do not log unsanitized user input
 
-public String sanitizeUser(String username) {
-    return Pattern.matches("[A-Za-z0-9_]+", username) ? username : "unauthorized user";
-}
-
-public class R00_IDS03_J {
-    if (loginSuccessful) {
-        logger.severe("User login succeeded for: " + sanitizeUser(username));
-    } else {
-        logger.severe("User login failed for: " + sanitizeUser(username));
-    }
-
-}
+if (loginSuccessful) {
+    logger.severe("User login succeeded for: " + username);
+  } else {
+    logger.severe("User login failed for: " + username);
+  }
--- R02_XP00_J.java
+++ R02_XP00_J.java
@@ -1,9 +1,9 @@
 // Rule 02. Expressions (EXP)
 // XP00-J. Do not ignore values returned by methods
-public void deleteFile() {
+public void deleteFile(){
+
     File someFile = new File("someFileName.txt");
     // Do something with someFile
-    if (!someFile.delete()) {
-        // Handle failure to delete the file
+    someFile.delete();
+
     }
-}
